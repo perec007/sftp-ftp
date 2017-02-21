3e9bf563dd6558e2d40806ed3c586b76b8783d73
Oleg Sidenko <oleg@casp.ru>
2017-02-21 20:33:54 +0300

    fix 0

--- start.sh
+++ start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-useradd -o -m  $USER -u $USERID
-echo -e "$PASS\n$PASS" | passwd $USER
-mkdir /home/$USER/.ssh/
-echo $PUBKEY > /home/$USER/.ssh/authorized_keys
-chown -R $USER.$USER /home/$USER
-chmod 0700 /home/$USER/.ssh/authorized_keys
-sed -i "s/'//g" /home/$USER/.ssh/authorized_keys
+
+if [ getent passwd $USER > /dev/null 2>&1 ]; then
+  useradd -m -o $USER -u $USERID
+  echo -e "$PASS\n$PASS" | passwd $USER
+
+  mkdir /home/$USER/.ssh
+  echo $PUBKEY > /home/$USER/.ssh/authorized_keys
+  chown -R $USER.$USER /home/$USER
+  chmod 0600 /home/$USER/.ssh/authorized_keys
+
+  sed -i "s/'//g" /home/$USER/.ssh/authorized_keys
+fi
+
 /usr/bin/python /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
