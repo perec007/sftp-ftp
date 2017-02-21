057d8930a7dc5c0fb08512fd6ac3842753ae534d
Oleg Sidenko <oleg@casp.ru>
2017-02-21 20:08:22 +0300

    fix 0

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-useradd -o -m  $USER -u $UID
+useradd -o -m  $USER -u $USERID
 echo -e "$PASS\n$PASS" | passwd $USER
+mkdir /home/$USER/.ssh/
 echo $PUBKEY > /home/$USER/.ssh/authorized_keys
-mkdir /home/$USER/.ssh
-touch /home/$USER/.ssh/authorized_keys
 chown -R $USER.$USER /home/$USER
 chmod 0700 /home/$USER/.ssh/authorized_keys
 sed -i "s/'//g" /home/$USER/.ssh/authorized_keys
