7caa48259e0e36a12aff632cc7f77dceef95658b
Oleg Sidenko <oleg@casp.ru>
2017-02-21 19:58:30 +0300

    fix 0

--- start.sh
+++ start.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-getent passwd $USER > /dev/null 2>&1
-
-if [ $? -ne 0 ]; then
-useradd $USER -u $UID
+useradd -o -m  $USER -u $UID
 echo -e "$PASS\n$PASS" | passwd $USER
-mkdir /home/$USER
-chown $USER.$USER /home/$USER
-chmod 0700 /home/$USER
+echo $PUBKEY > /home/$USER/.ssh/authorized_keys
 mkdir /home/$USER/.ssh
 touch /home/$USER/.ssh/authorized_keys
 chown -R $USER.$USER /home/$USER
 chmod 0700 /home/$USER/.ssh/authorized_keys
-echo $PUBKEY > /home/$USER/.ssh/authorized_keys
 sed -i "s/'//g" /home/$USER/.ssh/authorized_keys
-fi
 /usr/bin/python /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
